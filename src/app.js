8d2a22776810151d5487eab4fbb6bad5445a3109
Israel-Felipe <israelfelipet@gmail.com>
2022-09-14 21:32:08 -0300

    adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -9,4 +9,6 @@ app.use(express.json());
 
 app.use(userRouter);
 
-app.listen(5000, () => console.log("Listen server on port 5000"));
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
